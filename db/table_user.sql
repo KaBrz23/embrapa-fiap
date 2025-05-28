8f1409892e6231bd317486c33d5c52fb7c8a3ff1
KaBrz23 <kauefernandesbraz@gmail.com>
2025-05-27 22:32:21 -0300

    update

--- db/table_user.sql
+++ db/table_user.sql
@@ -1,6 +1,6 @@
 -- Script para criar tabela usuarios
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
